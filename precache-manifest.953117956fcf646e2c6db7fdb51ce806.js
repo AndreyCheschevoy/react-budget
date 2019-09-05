a190767da5480e9ddb8aab5fce9e9d8cb399d9a2
AndreyCheschevoy <a.cheschevoy@new-vist.com>
2019-09-05 15:02:50 +0300

    Updates

---
+++ precache-manifest.953117956fcf646e2c6db7fdb51ce806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf4ab8736f4ee70add023f51d682e73",
+    "url": "/react-budget/index.html"
+  },
+  {
+    "revision": "ca951f54252dbbe88a57",
+    "url": "/react-budget/static/css/main.76efd778.chunk.css"
+  },
+  {
+    "revision": "57a5e45cc36a970700f4",
+    "url": "/react-budget/static/js/2.66a65ba0.chunk.js"
+  },
+  {
+    "revision": "ca951f54252dbbe88a57",
+    "url": "/react-budget/static/js/main.28856ea3.chunk.js"
+  },
+  {
+    "revision": "8dfc80ba7989826d5fac",
+    "url": "/react-budget/static/js/runtime~main.0d51a4b3.js"
+  }
+]);
